c2e4e21c2a9282a84dc76ef4c7cc1b58acae76eb
gopi-ceq <121539215+gopi-ceq@users.noreply.github.com>
2023-01-06 09:13:59 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -18,13 +18,52 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  subnet_id     = "subnet-0c429fcd4ba09574e"
   key_name      = "gopi-ceq-gh-ssh-key"
+    
+  network_interface {
+  network_interface_id = aws_network_interface.foo.id
+  device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
 
   tags = {
     Name = var.ec2_name
   }
 }
+
